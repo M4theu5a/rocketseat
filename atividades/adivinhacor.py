a6cf4d25e83eee394c107825ee9f0786c6fd2f88
matheus.costa <matheus.costa@integrativa.com.br>
2025-12-14 22:46:27 -0300

    esquecidecomitar

---
+++ atividades/adivinhacor.py
@@ -0,0 +1,4 @@
+print('vou adivinhar a cor que você está pensando!')
+cor = input('Digite uma cor: ')
+input('estou pensei na cor, apere enter para ver se acertei')
+print(f'a cor que você pensou foi {cor}')
